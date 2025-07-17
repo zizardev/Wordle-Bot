586be850c41a0fe9a65635f31f6da1b879761ff6
Bogdan <bogdanmissss@gmail.com>
2025-07-17 23:10:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+import asyncio
+from aiogram import Bot, Dispatcher
 
+
+from app.handlers import router
+
+
+async def main():
+    bot = Bot(token='8143853300:AAE9VbLh4gqVu38OtxjTXofO9X3ivMprpXk')
+    dp = Dispatcher()
+    dp.include_router(router)
+    await dp.start_polling(bot)
+    print("alo")
+
+
+if __name__ == '__main__':
+    try:
+        print("alo")
+        asyncio.run(main())        
+    except KeyboardInterrupt:
+        print('Бот остановлен юзером')
